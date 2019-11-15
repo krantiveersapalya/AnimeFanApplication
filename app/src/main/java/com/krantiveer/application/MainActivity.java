9d8cb65c56438da54fcff03d9920df618dac092b
krantiveer.s@apalya.com <1234kveer>
2019-11-15 17:41:51 +0530

    instant app code working fine .. to upload we
    instant-release.zip ---->> instant-release>>base-release.apk

---
+++ app/src/main/java/com/krantiveer/application/MainActivity.java
@@ -0,0 +1,42 @@
+package com.krantiveer.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
+
+    public void dialog(View view) {
+        final String appPackageName = getPackageName(); // getPackageName() from Context or Activity
+        Log.d("kranti","hihihd "+appPackageName);
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
+    }
+
+    public void imagebanner(View view) {
+        final String appPackageName = getPackageName(); // getPackageName() from Context or Activity
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
+
+    }
+}
